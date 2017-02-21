1e27394d3e8381189cdc132c8b64a34232751a26
Luís Fonseca <fnsc.luis@gmail.com>
2017-02-21 19:48:09 -0300

    Create questao01.py

---
+++ lista_1/questao01.py
@@ -0,0 +1,6 @@
+# programa solicita dois numeros inteiros e imprime a soma
+n1 = int(input("Digite o valor do primeiro número: "))
+n2 = int(input("Digite o valor do segundo número: "))
+soma = n1 + n2
+
+print(soma)
